1e517690f31ff262dcd8e2e70c9d55186baf5af6
duongkibo <kibzz0zzz@gmail.com>
2023-07-06 23:29:37 +0700

    cDUONG: change package

--- android/app/src/main/kotlin/com/example/k8todonote/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.k8todonote
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/k8/todo/note/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.k8.todo.note
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
